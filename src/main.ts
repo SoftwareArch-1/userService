533a7114164e557748d442dd83ec6350deca156a
Sukrit Banthao <arm.xdsk@gmail.com>
2022-09-07 23:11:04 +0700

    📝 swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import { patchNestJsSwagger } from 'nestjs-zod'
+
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
+
+patchNestJsSwagger()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
