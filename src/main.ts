20af4c5b70000978253a5d46b7ffda811e63b3f1
extramix <sirapat.jr@gmail.com>
2022-09-21 10:31:23 +0700

    use cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
 
 patchNestJsSwagger()
 
@@ -19,6 +20,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
 
+  app.use(cookieParser())
   await app.listen(4000)
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
