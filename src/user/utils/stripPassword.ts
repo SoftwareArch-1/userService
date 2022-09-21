dc2371fd20423f205cc17890ee11daf64c667b4b
extramix <sirapat.jr@gmail.com>
2022-09-21 10:27:42 +0700

    remove entity type from stripPassword

--- src/user/utils/stripPassword.ts
+++ src/user/utils/stripPassword.ts
@@ -1,7 +1,7 @@
-import { UserT } from '../entities/user.entity'
+import { User } from '@prisma/client'
 import { SafeOmit } from './types'
 
-export const stripPassword = (user: UserT): SafeOmit<UserT, 'password'> => {
+export const stripPassword = (user: User) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { password, ...rest } = user
   return rest
