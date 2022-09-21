fc44ba0695750f58ec47ed80212ac38f68cf0f10
extramix <sirapat.jr@gmail.com>
2022-09-21 10:27:23 +0700

    add surname to create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -3,6 +3,7 @@ import { z } from 'nestjs-zod/z'
 
 export const createUserDtoSchema = z.object({
   name: z.string().min(3),
+  surname: z.string().min(3),
   email: z.string().email(),
   password: z.string().min(6),
 })
