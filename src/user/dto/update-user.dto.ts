7f5032fd402d01e7a304f9d0fc668f4887ce6136
Arm Sukrit <arm.xdsk@gmail.com>
2022-09-26 18:15:17 +0700

    🚧 added `description` and `birthDate` to `User` model

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,6 +1,14 @@
 import { createZodDto } from 'nestjs-zod'
+import { z } from 'nestjs-zod/z'
+
 import { createUserDtoSchema } from './create-user.dto'
 
-export const updateUserDtoSchema = createUserDtoSchema.partial()
+export const updateUserDtoSchema = createUserDtoSchema
+  .extend({
+    description: z.string().nullable(),
+    birthDate: z.dateString().nullable(),
+  })
+  .strict()
+  .partial()
 
 export class UpdateUserDto extends createZodDto(updateUserDtoSchema) {}
