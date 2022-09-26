7bd26108d1fbb637d6894265919e9ba7baded55b
Sukrit Banthao <arm.xdsk@gmail.com>
2022-09-26 21:37:54 +0700

    🩹 fixed importing and exporting services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
 import { ReviewModule } from './review/review.module'
+import { AppService } from './app.service'
 
 @Module({
   imports: [UserModule, AuthModule, ReviewModule],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
