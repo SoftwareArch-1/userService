49a7cc4ff8d2e93f5b780c626ab1bfabc17d39ff
Arm Sukrit <arm.xdsk@gmail.com>
2022-09-26 16:04:11 +0700

    🚧 `ReviewService.countReviewStars`

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import { User } from '@prisma/client'
 import { prismaClient } from 'prisma/script'
 import { CreateReviewDto } from './dto/createReview.dto'
 
@@ -27,5 +28,40 @@ export class ReviewService {
       },
     })
     return '👌'
+  }
+
+  async countReviewStars(userId: User['id']): Promise<{
+    1: number
+    2: number
+    3: number
+    4: number
+    5: number
+  }> {
+    const reviews = await prismaClient.review.findMany({
+      where: {
+        revieweeId: userId,
+      },
+      select: {
+        stars: true,
+      },
+    })
+
+    const result = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+    }
+
+    if (!reviews) {
+      return result
+    }
+
+    reviews.forEach(({ stars }) => {
+      result[stars]++
+    })
+
+    return result
   }
 }
