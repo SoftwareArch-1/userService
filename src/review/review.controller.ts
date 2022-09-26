7352906c89022c98cc9708015e86d49707573301
Arm Sukrit <arm.xdsk@gmail.com>
2022-09-26 15:43:00 +0700

    🚧 endpoint to create a new review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { ReviewService } from './review.service';
+import { Body, Controller, Post, Req } from '@nestjs/common'
+import { Request } from 'express'
+import { UseZodGuard } from 'nestjs-zod'
+import { CreateReviewDto } from './dto/createReview.dto'
+import { ReviewService } from './review.service'
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @Post()
+  @UseZodGuard('body', CreateReviewDto)
+  create(@Req() req: Request, @Body() body: CreateReviewDto) {
+    return this.reviewService.create({
+      ...body,
+      reviewerId: '' /* req.user?.id */,
+    })
+  }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common'
+import { prismaClient } from 'prisma/script'
+import { CreateReviewDto } from './dto/createReview.dto'
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  async create({
+    content,
+    revieweeId,
+    stars,
+    reviewerId,
+  }: CreateReviewDto & {
+    reviewerId: string // from request object
+  }) {
+    await prismaClient.user.update({
+      where: {
+        id: revieweeId,
+      },
+      data: {
+        reviews: {
+          create: {
+            content,
+            stars,
+            reviewerId,
+          },
+        },
+      },
+    })
+    return '👌'
+  }
+}
