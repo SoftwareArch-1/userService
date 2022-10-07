96df6199850db5f5612dac2ce3d9e4cbf444f1f6
Sukrit Banthao <arm.xdsk@gmail.com>
2022-10-07 10:26:43 +0700

    🏷️ [abcde] updated activity types
    search for [abcde] in activity service repo to find the related commit

--- src/sync_with_activity/activity.proto.interface.ts
+++ src/sync_with_activity/activity.proto.interface.ts
@@ -1,18 +1,29 @@
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
 
 export interface ActivityService {
-  findOne: (data: ActivityById) => Observable<Activity>
-  findMany(data: FindManyParams): Observable<Activity>
+  findOne: (data: ActivityById) => Observable<Activity>;
+  findMany(data: FindManyParams): Observable<Activity>;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface FindManyParams {}
 
 export interface ActivityById {
-  id: string
+  id: string;
 }
 
 export interface Activity {
-  id: string
-  detail: string
+  id: string;
+  name: string;
+  description: string;
+  owner_id: string;
+  target_date_iso_string: string;
+  max_participants: number;
+  require_line: boolean;
+  require_discord: boolean;
+  tag: string;
+  location?: string;
+
+  joined_user_ids?: string[];
+  pending_user_ids?: string[];
 }
