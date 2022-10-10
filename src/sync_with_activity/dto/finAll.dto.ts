54b52c65a9e0f022ce39839e18d302c91cd621e7
Sukrit Banthao <arm.xdsk@gmail.com>
2022-10-10 22:25:09 +0700

    🚧 `eachInAll` in activity's find all dto also returns `joinedUserIds`

--- src/sync_with_activity/dto/finAll.dto.ts
+++ src/sync_with_activity/dto/finAll.dto.ts
@@ -2,7 +2,6 @@ import { ActivityModel } from '../zod'
 
 export const eachInAll = ActivityModel.omit({
   pendingUserIds: true,
-  joinedUserIds: true,
 })
 
 export const findAllActivityDto = ActivityModel.array()
