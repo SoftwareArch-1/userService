d81a0f2cfacef77aa990621921fc1e07c8364631
Sukrit Banthao <arm.xdsk@gmail.com>
2022-10-11 23:24:29 +0700

    🚧 patch user no longer accepts param `:id`

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -23,6 +23,7 @@ import { UpdateUserDto } from './dto/update-user.dto'
 import { UserService } from './user.service'
 import { HttpStatus } from '@nestjs/common/enums'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
+import { AuthenticatedRequest } from 'src/types'
 
 @Controller('user')
 @ApiTags('user')
@@ -70,10 +71,13 @@ export class UserController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Patch(':id')
+  @Patch()
   @UseZodGuard('body', UpdateUserDto)
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto)
+  update(
+    @Body() updateUserDto: UpdateUserDto,
+    @Req() req: AuthenticatedRequest,
+  ) {
+    return this.userService.update(req.user.id, updateUserDto)
   }
 
   // @Delete(':id')
