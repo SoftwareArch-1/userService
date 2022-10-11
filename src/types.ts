bb124dab12213735db5e1eb4b4893d5cfac324c6
Sukrit Banthao <arm.xdsk@gmail.com>
2022-10-11 22:37:27 +0700

    🏷️ fix user type in `AuthenticatedRequest`

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
-import { User } from '@prisma/client'
 import { Request } from 'express'
 
 export interface AuthenticatedRequest extends Request {
-  user: User
+  user: {
+    email: string
+    id: string
+  }
 }
