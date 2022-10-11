260c2c324ccbe36846ddc149abc868185923e34e
extramix <sirapat.jr@gmail.com>
2022-10-11 14:58:41 +0700

    return profile data when login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,9 +22,10 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, sub: user.id }
+    const payload = { email: user.email }
 
     return {
+      user: await this.userService.findMe(user.email),
       access_token: this.jwtService.sign(payload),
     }
   }
