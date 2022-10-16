11160753051368f09155a03690fc36c9ad0e89b0
Sukrit Banthao <arm.xdsk@gmail.com>
2022-10-16 23:24:38 +0700

    🚧 handle socket connection

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,15 +1,29 @@
 import {
   MessageBody,
+  OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets'
+import { Socket } from 'socket.io'
 
 import { ChatService } from './chat.service'
 import { WsRes } from './dto/base'
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
   constructor(private readonly chatService: ChatService) {}
+
+  handleConnection(client: Socket) {
+    const { userId } = client.handshake.query
+    if (typeof userId !== 'string') {
+      client.send(
+        'Send userId as query parameter when establishing connection, like this, io("...", { query: { userId: "userId" }})',
+      )
+      client.disconnect()
+      return
+    }
+    client.data.userId = userId
+  }
 
   @SubscribeMessage('echo')
   echo(@MessageBody() data: string): WsRes<typeof data> {
