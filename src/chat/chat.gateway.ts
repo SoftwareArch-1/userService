ef0bcb3dda80ef6adcc1b6fa1c0c9938f317c23f
Sukrit Banthao <arm.xdsk@gmail.com>
2022-10-16 22:45:25 +0700

    🚧 message `echo`

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,16 @@
-import { WebSocketGateway } from '@nestjs/websockets';
-import { ChatService } from './chat.service';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets'
+import { ChatService } from './chat.service'
 
 @WebSocketGateway()
 export class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
+
+  @SubscribeMessage('echo')
+  echo(@MessageBody() data: string): string {
+    return data
+  }
 }
